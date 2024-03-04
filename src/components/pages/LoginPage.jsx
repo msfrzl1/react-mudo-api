7f489a21694bf58d1e8303debe299aa59686d546
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-04 18:59:53 +0700

    implemented login functionality with axios for API calls

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -4,10 +4,12 @@ import Navbar from "../Fragments/Navbar";
 import { useState } from "react";
 import Label from "../Elements/Label";
 import Input from "../Elements/Input";
+import axios from "axios";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [notif, setNotif] = useState("");
 
   const handleUsername = (e) => {
     setUsername(e.target.value);
@@ -15,6 +17,24 @@ const LoginPage = () => {
 
   const handlePassword = (e) => {
     setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    const payload = {
+      username: username,
+      password: password,
+    };
+
+    axios
+      .post("https://api.mudoapi.tech/login", payload)
+      .then((res) => {
+        const token = res.data.data.token;
+        localStorage.setItem("access", token);
+        setNotif({ message: res.data.message, success: true });
+      })
+      .catch((error) => {
+        setNotif({ message: error.response.data.message, success: false });
+      });
   };
 
   return (
@@ -43,9 +63,13 @@ const LoginPage = () => {
               <button
                 type="submit"
                 className="w-full py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ease-in-out bg-indigo-600 hover:bg-indigo-700"
+                onClick={handleLogin}
               >
                 Login
               </button>
+              {notif.message && (
+                <p className={notif.success ? "text-green-500 flex justify-center mt-2" : "text-red-500 flex justify-center mt-2"}>{notif.message}</p>
+              )}
             </div>
           </div>
         </div>
