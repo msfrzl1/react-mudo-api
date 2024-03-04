5b87dfbc70b2786407d4d5952318e30b9984700d
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-04 16:01:17 +0700

    correct route page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
-import MenuPage from "./components/MenuPage";
-import MenuDetailPage from "./components/MenuDetailPage";
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
+import MenuPage from "./components/pages/MenuPage";
+import MenuDetailPage from "./components/pages/MenuDetailPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="/menu" element={<MenuPage />}></Route>
-        <Route path="/menu-detail" element={<MenuDetailPage />}></Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/menu-detail" element={<MenuDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
