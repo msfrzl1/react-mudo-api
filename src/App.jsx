0fedbbf9a76dd35af6195331d2f5adbb5d329946
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-03-04 15:55:58 +0700

    create route to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+import MenuPage from "./components/MenuPage";
+import MenuDetailPage from "./components/MenuDetailPage";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/menu" element={<MenuPage />}></Route>
+        <Route path="/menu-detail" element={<MenuDetailPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
